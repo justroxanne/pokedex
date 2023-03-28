8e141a1685d1b6110fe31cf734b97737b4a5084d
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-03-28 12:56:45 +0200

    exercise components 2 done

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,27 @@
 function PokemonCard() {
-    return <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="picture of Bulbasaur" /><figcaption>Bulbasaur</figcaption></figure>;
+    const pokemon = pokemonList[1];
+    return (
+        <figure>
+            { pokemon.imgSrc != undefined ?
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="picture of Bulbasaur" />
+            :<p>???</p>
+            }
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>);
 }
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
+
 
 export default PokemonCard;
