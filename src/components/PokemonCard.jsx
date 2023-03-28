3c2f962f3b1fd176d8b29cc464ce8c6ab1a7c64c
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-03-28 11:23:16 +0200

    first pokemonCard added

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,5 @@
+function PokemonCard() {
+    return <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="picture of Bulbasaur" /><figcaption>Bulbasaur</figcaption></figure>;
+}
+
+export default PokemonCard;
