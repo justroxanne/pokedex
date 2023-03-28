e6007c728fc5b1ff0acb766b07670f45730eeb84
Roxanne Lucas <114505137+justroxanne@users.noreply.github.com>
2023-03-28 20:26:14 +0200

    Update Apps.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
 import './App.css'
 import PokemonCard from "./components/PokemonCard"
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
 function App() {
   const pokemonList = [
