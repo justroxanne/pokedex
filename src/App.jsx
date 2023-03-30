b5c6910a96e554650ebcb8630fc018db4063c5ee
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-03-30 12:25:44 +0200

    added a render list of all pokemons

--- src/App.jsx
+++ src/App.jsx
@@ -8,36 +8,43 @@ const pokemonList = [
       name: "bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      type: "grass",
     },
     {
       name: "charmander",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      type: "fire",
     },
     {
       name: "squirtle",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      type: "water",
     },
     {
       name: "pikachu",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      type: "electrik",
     },
     {
       name: "mew",
+      type: "psy",
     },
   ]; 
 
 function App() {
   const [pokemonIndex, setPokemonIndex] = useState(0);
 
-  return (<div>
+  return (<div className="pokemonBox">
     <div className="pokeball">
       <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
     </div>
-    <div>
-      <NavBar setPokemonIndex={setPokemonIndex} pokemonIndex={pokemonIndex} pokemonList={pokemonList}/>
+    <div className="navbar">
+      {pokemonList.map((pokemonIndex, index) =>
+      <NavBar setPokemonIndex={setPokemonIndex} pokemonIndex={pokemonIndex} index={index}/>
+      )}
     </div>
     </div>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 
-const NavBar = ({pokemonIndex, setPokemonIndex, pokemonList}) => {
+const NavBar = ({pokemonIndex, setPokemonIndex, index}) => {
 
-    const handleClickPrevious = () => {
-        setPokemonIndex(pokemonIndex -1);
+    const handleClick = () => {
+        setPokemonIndex(pokemonIndex = index);
     }
-    const handleClickNext = () => {
-        setPokemonIndex(pokemonIndex + 1);
-    }
+
+    // const handleClickPrevious = () => {
+    //     setPokemonIndex(pokemonIndex -1);
+    // }
+    // const handleClickNext = () => {
+    //     setPokemonIndex(pokemonIndex + 1);
+    // }
     
     return (
         <div>
-            {pokemonIndex > 0 ? <button className="button" onClick={handleClickPrevious}>Previous</button> : <button>No more item</button>}
-            {pokemonIndex < pokemonList.length -1 ? <button className="button" onClick={handleClickNext}>Next</button> : <button>No more item</button>}
+            <button className="button" onClick={handleClick} key={pokemonIndex.name}>{pokemonIndex.name}</button>
         </div>
+
+
+        // <div>
+        //     {pokemonIndex > 0 ? <button className="button" onClick={handleClickPrevious}>Previous</button> : <button>No more item</button>}
+        //     {pokemonIndex < pokemonList.length -1 ? <button className="button" onClick={handleClickNext}>Next</button> : <button>No more item</button>}
+        // </div>
     );
 };
 
